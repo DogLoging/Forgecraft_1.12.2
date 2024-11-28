6564595caac0fe36e0cccdb371f3b8446fc37a21
Guilherme Hyde <Guilherme Hyde@DESKTOP-07VSMC6>
2024-11-28 19:33:54 -0300

    project fusion furnace

--- forge-1.12.2-14.23.5.2859-mdk/src/main/java/com/modding/forgecraft/blocks/BlockFusionFurnace.java
+++ forge-1.12.2-14.23.5.2859-mdk/src/main/java/com/modding/forgecraft/blocks/BlockFusionFurnace.java
@@ -1,14 +1,48 @@
-package com.modding.forgecraft.block;
+package com.modding.forgecraft.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockFusionFurnace extends Block
 {
+	public static final PropertyDirection FACING = BlockHorizontal.FACING;
+	
 	public BlockFusionFurnace()
 	{
 		super(Material.IRON);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 	}
 	
+	@Override
+	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+	{
+		return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+	}
 	
+	@Override
+	public IBlockState getStateFromMeta(int meta)
+	{
+		EnumFacing facing = EnumFacing.getHorizontal(meta);
+		return this.getDefaultState().withProperty(FACING, facing);
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state)
+	{
+		return state.getValue(FACING).getHorizontalIndex(); 
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState()
+	{
+		return new BlockStateContainer(this, FACING);
+	}
 }
