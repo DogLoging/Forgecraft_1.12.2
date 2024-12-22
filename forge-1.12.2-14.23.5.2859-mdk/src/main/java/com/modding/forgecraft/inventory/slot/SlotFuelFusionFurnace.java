a469c728b499c3469f76125fc4cdbc05c5cbe9a4
Guilherme Hyde <Guilherme Hyde@DESKTOP-07VSMC6>
2024-12-22 20:39:19 -0300

    Final Project Fusion Furnace

--- forge-1.12.2-14.23.5.2859-mdk/src/main/java/com/modding/forgecraft/inventory/slot/SlotFuelFusionFurnace.java
+++ forge-1.12.2-14.23.5.2859-mdk/src/main/java/com/modding/forgecraft/inventory/slot/SlotFuelFusionFurnace.java
@@ -6,7 +6,6 @@ import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntityFurnace;
 
 public class SlotFuelFusionFurnace extends Slot
 {
