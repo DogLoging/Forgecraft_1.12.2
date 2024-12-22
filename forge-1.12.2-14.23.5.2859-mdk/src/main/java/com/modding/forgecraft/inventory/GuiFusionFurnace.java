8f92b08645fd02a29ba23a0756a3c106526b5e00
Guilherme Hyde <Guilherme Hyde@DESKTOP-07VSMC6>
2024-12-22 08:53:50 -0300

    Fix name

--- forge-1.12.2-14.23.5.2859-mdk/src/main/java/com/modding/forgecraft/inventory/GuiFusionFurnace.java
+++ forge-1.12.2-14.23.5.2859-mdk/src/main/java/com/modding/forgecraft/inventory/GuiFusionFurnace.java
@@ -25,6 +25,15 @@ public class GuiFusionFurnace extends GuiContainer
     }
     
     @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks)
+    {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+
+        this.renderHoveredToolTip(mouseX, mouseY);
+    }
+    
+    @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
     {
         String s = tileFusion.getDisplayName().getUnformattedText();
@@ -32,10 +41,8 @@ public class GuiFusionFurnace extends GuiContainer
         fontRenderer.drawString(s, xSize / 2 - fontRenderer.getStringWidth(s) / 2, 6, 4210752);
         fontRenderer.drawString(player.getDisplayName().getUnformattedText(), 8, ySize - 96 + 2, 4210752);
         
-        int marginHorizontal = (width - xSize) / 2;
-        int marginVertical = (height - ySize) / 2;
-        
         String fuelText = this.tileFusion.getField(5) + " / " + 5000;
+        
         fontRenderer.drawString(fuelText, 50, 44, 0x7d7d7d);
     }
 
